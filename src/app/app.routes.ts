43216a7d886541cb7d8109377e4705aa02c352ea
Elomba Elmine <elmine0520@gmail.com>
2026-04-02 13:29:02 +0100

    my first angular project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Hello } from './components/hello/hello';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'hello', component: Hello },
+    {path: '', component: App},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';  
+import { Body } from './components/body/body';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ Header, Body], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('firstProject');
+  protected readonly title = signal('miss_joy');
 }
